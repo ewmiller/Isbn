f7b7ad4b198a47d81a969d1153b66e0cb1e90fb1
Ethan Miller <ethan.w.miller@gmail.com>
2015-12-30 11:53:23 -0500

    finished the TODOs

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A simple ISBN-10 validator. Written in Java using IntelliJ IDEA. Takes an ISBN n
 command line the result of the evaluation.
 Created for [this dailyprogrammer challenge](https://www.reddit.com/r/dailyprogrammer/comments/2s7ezp/20150112_challenge_197_easy_isbn_validator/)
 on Reddit.
+
+## Usage
+At runtime, the program will ask for user input. Here is an example of an ISBN that is valid: 0385539436. An invalid
+example would be: 1234567890.
--- src/Isbn.java
+++ src/Isbn.java
@@ -1,34 +1,52 @@
 /**
  * Created by Ethan Miller on 12/30/15.
  */
+
+import java.util.*;
+
 public class Isbn {
     public String number;
     public Isbn(String number){
         this.number = number;
     }
     public static void main(String[] args){
-        //TODO: get number as user input
-        Isbn isbn = new Isbn("0385539436");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an ISBN-10 number into the console, excluding dashes (e.g. 0385539436):");
+        String num = scanner.nextLine();
+        Isbn isbn = new Isbn(num);
         if(isbn.isValid()){
-            System.out.println("ISBN number " + isbn.number + " is valid.");
+            System.out.println("ISBN " + isbn.number + " is valid.");
         }
         else {
-            System.out.println("ISBN number " + isbn.number + " is not valid.");
+            System.out.println("ISBN " + isbn.number + " is not valid.");
         }
     }
 
     public boolean isValid(){
         char[] arr = number.toCharArray();
+        if(arr.length != 10){
+            System.out.println("Invalid input: " + number + " is not the correct length.");
+            System.out.println("Exiting.");
+            System.exit(1);
+        }
         int[] iArr = new int[arr.length];
         int sum = 0;
 
-        //TODO: validate number string, or add error checking
-        //TODO: handle "X" digit
-
         // cast char array to int array
-        for(int i = 0; i < arr.length; i++){
-            String s = Character.toString(arr[i]);
-            iArr[i] = Integer.parseInt(s);
+        try {
+            for (int i = 0; i < arr.length; i++) {
+                String s = Character.toString(arr[i]);
+                // handles X digit that can appear in ISBN numbers
+                if (s.equals("X")) {
+                    iArr[i] = 10;
+                } else {
+                    iArr[i] = Integer.parseInt(s);
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error while reading input: " + number);
+            System.out.println(e.toString());
+            System.out.println("Your input may be incorrectly formatted.");
         }
         // sum operation according to isbn rules
         for(int i = 10; i > 0; i--){
